fe0b45e61cd8c1876f63cf2a5f820d4a7f9b5e55
Berkay <240208453@ostimteknik.edu.tr>
2026-07-13 15:58:08 +0300

    New destroy method for deleting an existing user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to users_path, notice: "Kullanıcı silindi."
+  end
+
   private
 
   def user_params
