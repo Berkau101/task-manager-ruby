46312cff8598067a50d0bdeb868e72f2a314800f
Berkay <240208453@ostimteknik.edu.tr>
2026-07-24 11:04:04 +0300

    Delegate email validation to Devise in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   has_many :tasks, dependent: :destroy
 
   validates :name, presence: true
-  validates :email, presence: true,
-  uniqueness: true,
-  format: { with: URI::MailTo::EMAIL_REGEXP, message: "geçerli bir e-posta olmalı" }
+  # validates :email, presence: true,
+  # uniqueness: true,
+  # format: { with: URI::MailTo::EMAIL_REGEXP, message: "geçerli bir e-posta olmalı" }
 end
