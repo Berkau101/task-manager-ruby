c99cf9ff0f4ffd66c72c9650585af40a1c62777c
Berkay <240208453@ostimteknik.edu.tr>
2026-07-24 11:03:16 +0300

    update schema.rb after Devise migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_09_115812) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_22_083437) do
   create_table "tasks", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "description"
@@ -25,9 +25,14 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_09_115812) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "name", null: false
+    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "tasks", "users"
